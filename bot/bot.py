fde6c5c8b26c849d1b23239dc588aef16c6516a7
abaycom <158740646+abaycom@users.noreply.github.com>
2026-07-13 02:20:26 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -11,11 +11,17 @@ from telegram.ext import (
     ContextTypes
 )
 
+from database import (
+    create_tables,
+    add_user
+)
 
-BOT_TOKEN = "5980643111:AAGboEhnGpZ1rYsJl7i4W9NVg3XD8upuD0E"
 
+
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
 
 WEB_APP_URL = "https://your-domain.com"
+
 
 
 async def start(
@@ -26,68 +32,112 @@ async def start(
     user = update.effective_user
 
 
+    # Save User
+
+    add_user(
+        user.id,
+        user.username,
+        user.first_name
+    )
+
+
+
     keyboard = [
 
         [
 
-            InlineKeyboardButton(
-                "🎬 Open AbayFlix",
-                web_app=WebAppInfo(
-                    url=WEB_APP_URL
-                )
+        InlineKeyboardButton(
+
+            "🎬 Open AbayFlix",
+
+            web_app=WebAppInfo(
+                url=WEB_APP_URL
             )
+
+        )
 
         ]
 
     ]
 
 
-    reply_markup = InlineKeyboardMarkup(
-        keyboard
-    )
-
-
     await update.message.reply_text(
 
         f"""
-🎬 እንኳን ወደ AbayFlix በደህና መጡ!
+🎬 Welcome to AbayFlix
 
 👤 {user.first_name}
 
 ምርጥ ፊልሞችን እና
-ተከታታይ ፊልሞችን ይመልከቱ።
+ተከታታይ ፊልሞችን ይመልከቱ.
 
 """,
 
-        reply_markup=reply_markup
+        reply_markup=
+        InlineKeyboardMarkup(keyboard)
 
     )
 
 
 
 
-async def help_command(
+
+
+# ADMIN
+
+ADMIN_ID = 123456789
+
+
+
+
+async def users_count(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE
 ):
 
+
+    if update.effective_user.id != ADMIN_ID:
+
+        return
+
+
+
+    from database import connect
+
+
+    con = connect()
+
+    cur = con.cursor()
+
+
+    cur.execute(
+        "SELECT COUNT(*) FROM users"
+    )
+
+
+    count = cur.fetchone()[0]
+
+
+    con.close()
+
+
+
     await update.message.reply_text(
 
-        """
-🎬 AbayFlix Help
-
-/start - Open App
-
-ከተጨማሪ አገልግሎት ጋር ይጠብቁ።
-"""
+        f"👥 Total Users: {count}"
 
     )
+
+
 
 
 
 
 
 def main():
+
+    create_tables()
+
 
     app = Application.builder()\
     .token(BOT_TOKEN)\
@@ -108,8 +158,8 @@ def main():
     app.add_handler(
 
         CommandHandler(
-            "help",
-            help_command
+            "users",
+            users_count
         )
 
     )
@@ -117,7 +167,7 @@ def main():
 
 
     print(
-        "AbayFlix Bot Started..."
+        "AbayFlix Bot Running..."
     )
 
 
@@ -126,6 +176,6 @@ def main():
 
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
 
     main()
