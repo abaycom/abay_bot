da5df69ba31bb3e0f56627f2c3e641875d80af01
abaycom <158740646+abaycom@users.noreply.github.com>
2026-07-25 11:28:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from aiogram.enums import ParseMode
 
 # ==================== CONFIGURATION ====================
 # ቦት ቶከንዎን እና የዌብ አፕ ሊንክዎን እዚህ ይተኩ ወይም በ Railway Environment Variables ውስጥ ያስገቡ
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8616146622:AAFnfw1ltj-TWT3_UvceYwmWpuKbqcX_fSM")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8650486535:AAGEBJA2DJjIy2NkiVFnYn4HlhDmiclSRUU")
 WEBAPP_URL = os.getenv("WEBAPP_URL", "https://abaybot-production.up.railway.app")
 PORT = int(os.getenv("PORT", 8080))
 ADMIN_IDS = [5049565154]
